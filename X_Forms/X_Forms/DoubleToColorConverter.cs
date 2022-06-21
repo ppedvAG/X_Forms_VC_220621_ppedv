cd5f1663bd7d7b93f7b056b36239047408f4fd61
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-06-21 12:35:42 +0200

    Add project files.

---
+++ X_Forms/X_Forms/DoubleToColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace X_Forms
+{
+    internal class DoubleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.FromRgb((byte)(double)value, System.Convert.ToByte(parameter), 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace X_Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Application.Current.Resources["GlobalFontSize"] = 25.0;
+
+            InitializeComponent();
+
+            Sly_Main.Resources["BtnString"] = "Neuer String";            
+        }
+
+        private void Btn_KlickMich_Clicked(object sender, EventArgs e)
+        {
+            Lbl_Output.Text = "Glückwunsch";
+
+            (sender as Button).Text = "Button wurde geklickt";
+        }
+
+        private void Ent_Input_Completed(object sender, EventArgs e)
+        {
+            Lbl_Output.Text = Ent_Input.Text;
+        }
+
+        private void IBtn_Beispiel_Clicked(object sender, EventArgs e)
+        {
+            Ent_Input.Text = Pkr_Names.SelectedItem?.ToString();
+        }
+    }
+}
