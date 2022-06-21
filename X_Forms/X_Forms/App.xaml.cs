8868404ba39aaad04a1c8127857ad862b477d1b5
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-06-21 16:54:21 +0200

    Absolute-/ RelativeLayout

--- X_Forms/X_Forms/App.xaml.cs
+++ X_Forms/X_Forms/App.xaml.cs
@@ -15,7 +15,7 @@ namespace X_Forms
             //Zuweisung einer Page zu der MainPage-Property (Startseite)
             //MainPage = new MainPage();
 
-            MainPage = new Übungen.U_GridLayout();
+            MainPage = new Übungen.U_AbsoluteLayout();
         }
 
         protected override void OnStart()
---
+++ X_Forms/X_Forms/Layouts/RelativeLayoutBsp.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace X_Forms.Layouts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RelativeLayoutBsp : ContentPage
+    {
+        public RelativeLayoutBsp()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Box_Clicked(object sender, EventArgs e)
+        {
+            //Bei Neuzuweisung der Constraints muss durch die Constraint-Klasse eine neue Berechnungslogik für den Wert übergeben werden.
+            //Hier: Y-Position ergibt sich zukünftig aus 10% der Breite des Parents (des umgebenden RelativLayouts)
+            RelativeLayout.SetYConstraint(Bxv_Green, Constraint.RelativeToParent(parent => parent.Width * 0.1));
+            //RelativeLayout.SetYConstraint(Bxv_Green, Constraint.RelativeToParent(Verschiebe));
+        }
+
+        private double Verschiebe(RelativeLayout parent)
+        {
+            return parent.Width * 0.1;
+        }
+    }
+}
