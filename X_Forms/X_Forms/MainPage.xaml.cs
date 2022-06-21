bc26e73972a07846c3eca5fcf9dcd5ba797fc460
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-06-21 15:07:05 +0200

    Bindings, Layouts

--- X_Forms/X_Forms/MainPage.xaml.cs
+++ X_Forms/X_Forms/MainPage.xaml.cs
@@ -12,11 +12,12 @@ namespace X_Forms
     {
         public MainPage()
         {
+            //Codeseitige Veränderung einer Ressource in der App.xaml (bzw hier im ResourceDictionary, das in der App.xaml angemeldet ist-->
             Application.Current.Resources["GlobalFontSize"] = 25.0;
 
             InitializeComponent();
 
-            Sly_Main.Resources["BtnString"] = "Neuer String";            
+            (this.Content as StackLayout).Resources["BtnString"] = "Neuer String";
         }
 
         private void Btn_KlickMich_Clicked(object sender, EventArgs e)
@@ -31,9 +32,48 @@ namespace X_Forms
             Lbl_Output.Text = Ent_Input.Text;
         }
 
+        //EventHandler eines Slider-ValueChanged-Events (reagiert auf Wert-Veränderung in Slider.Value)
+        private void Sdr_Wert_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Sdr_Wert.Value = Math.Round(Sdr_Wert.Value);
+        }
+
         private void IBtn_Beispiel_Clicked(object sender, EventArgs e)
         {
             Ent_Input.Text = Pkr_Names.SelectedItem?.ToString();
+        }
+
+        private void Btn_Show_Clicked(object sender, EventArgs e)
+        {
+            Person person = Sly_DataBinding.BindingContext as Person;
+            DisplayAlert("Person:", $"{person.Name} ({person.Alter})", "Ok");
+        }
+
+        private void Btn_Altern_Clicked(object sender, EventArgs e)
+        {
+            Person person = Sly_DataBinding.BindingContext as Person;
+            person.Alter++;
+        }
+
+        private void Btn_Add_Clicked(object sender, EventArgs e)
+        {
+            Person person = Sly_DataBinding.BindingContext as Person;
+            person.WichtigeTage.Add(DateTime.Now);
+        }
+
+        private void Btn_Delete_Clicked(object sender, EventArgs e)
+        {
+            if (LstV_Tage.SelectedItem != null)
+            {
+                DateTime tag = (DateTime)LstV_Tage.SelectedItem;
+                (Sly_DataBinding.BindingContext as Person).WichtigeTage.Remove(tag);
+            }
+        }
+
+        private async void MItm_Delete_Clicked(object sender, EventArgs e)
+        {
+            if (await DisplayAlert("Löschen", "Möchtest du diesen Tag wirklich löschen?", "Ja", "Nein"))
+                (Sly_DataBinding.BindingContext as Person).WichtigeTage.Remove((DateTime)(sender as MenuItem).CommandParameter);
         }
     }
 }
---
+++ X_Forms/X_Forms/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace X_Forms
+{
+    internal class Person : INotifyPropertyChanged
+    {
+
+        public string Name { get; set; }
+
+        private int alter;
+
+
+        public int Alter
+        {
+            get => alter;
+            set 
+            { 
+                alter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            }
+        }
+
+        public ObservableCollection<DateTime> WichtigeTage { get; set; } = new ObservableCollection<DateTime>()
+        {
+            new DateTime(2003, 12,1),
+            new DateTime(2006, 11,11),
+            new DateTime(2012, 10,12),
+            new DateTime(2022, 1,13),
+        };
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
